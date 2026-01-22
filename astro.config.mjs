3cf14870088174b1ff850e805f3d74383e8c3dbd
neps-influx <nathaniel.epstein@influxmarketing.com>
2026-01-22 15:21:08 -0500

    output structure fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from"@astrojs/cloudflare";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  output: 'server', // tell astro this isnt a static site
-  adapter: cloudflare({ // tell astro to build for cloudflare
-    mode: 'advanced' // use advanced mode for cloudflare workers
-  })
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
