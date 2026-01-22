9e55b8ef11e6d431865dff22cbaf017853d1c881
neps-influx <nathaniel.epstein@influxmarketing.com>
2026-01-22 15:14:33 -0500

    fixed astro file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from"@astrojs/cloudflare";
+
 export default defineConfig({
   output: 'server', // tell astro this isnt a static site
-  adapter: cloudflare() // tell astro to build for cloudflare
+  adapter: cloudflare({ // tell astro to build for cloudflare
+    mode: 'advanced' // use advanced mode for cloudflare workers
+  })
 });
