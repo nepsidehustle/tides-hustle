94cfe47f967a605fa7a6a03a85824a955293721f
neps-influx <nathaniel.epstein@influxmarketing.com>
2026-01-22 20:44:50 -0500

    refresh

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(), // No extra modes or proxies needed for now
-});
+  trailingSlash: 'always', //force remap of all routes
+}); 
