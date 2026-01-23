942c86f94f23a9dc53e126213e139ed10aba2ad5
neps-influx <nathaniel.epstein@influxmarketing.com>
2026-01-23 16:40:52 -0500

    graph clean up3

--- src/pages/api/tide-data.ts
+++ src/pages/api/tide-data.ts
@@ -9,9 +9,9 @@ export async function GET() {
   try {
     const allData = await Promise.all(
       stations.map(async (site) => {
-        // CHANGED: range=72. This ensures we have 3 days of data.
-        // Even if it's 10 PM, we still have 48+ hours of future data buffered.
-        const api = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?station=${site.id}&date=today&range=72&product=predictions&datum=MLLW&time_zone=lst_ldt&units=english&interval=h&format=json&cb=${Date.now()}`;
+        // CHANGED: range=96 (4 Days). 
+        // This ensures the chart always has 48+ hours of future data to draw.
+        const api = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?station=${site.id}&date=today&range=96&product=predictions&datum=MLLW&time_zone=lst_ldt&units=english&interval=h&format=json&cb=${Date.now()}`;
         
         const res = await fetch(api);
         const json = await res.json();
